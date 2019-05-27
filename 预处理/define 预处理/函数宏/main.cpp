619fa1f6270804cb1437780fc012cf5df599a21a
zhumingwei <zhumingwei@wallstreetcn.com>
2019-05-27 19:08:01 +0800

    first commit

---
+++ 预处理/define 预处理/函数宏/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+#define MIN(a,b) (((a)<(b))?a:b)
+int main(){
+    int i,j;
+    i = 100;
+    j = 30;
+    cout << "the minimum is " << MIN(i,j) << endl;
+    return 0;
+}
